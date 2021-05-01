45a7b22dcc21df24a3c5df17745cee4b8fa194f7
Martin Disch <martindisch@gmail.com>
2021-05-01 13:50:46 +0200

    Read input files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{fs, io};
+
 fn main() {
-    println!("Hello, world!");
+    let mut files = fs::read_dir("data/input/")
+        .expect("Could not read input files")
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()
+        .expect("Could not map entries to paths");
+    files.sort();
+
+    println!("{:?}", files);
 }
