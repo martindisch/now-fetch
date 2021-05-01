ca53259bde82e53025e5e058c0061d8cf184399c
Martin Disch <martindisch@gmail.com>
2021-05-01 13:54:01 +0200

    Use eyre for error context

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use eyre::{Result, WrapErr};
 use std::{fs, io};
 
-fn main() {
+fn main() -> Result<()> {
     let mut files = fs::read_dir("data/input/")
-        .expect("Could not read input files")
+        .wrap_err("Could not read input files")?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()
-        .expect("Could not map entries to paths");
+        .wrap_err("Could not map entries to paths")?;
     files.sort();
 
     println!("{:?}", files);
+    Ok(())
 }
