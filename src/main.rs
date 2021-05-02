d3c254e065211c2c82e9fe19cbe0a72a78648cd5
Martin Disch <martindisch@gmail.com>
2021-05-02 14:58:39 +0200

    Download audio files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use eyre::{eyre, Result, WrapErr};
 use quick_xml::de;
+use reqwest::blocking::Client;
 use std::path::Path;
 use std::{
     convert::{TryFrom, TryInto},
@@ -15,6 +16,8 @@ use table::{Expression, Flashcard, Table};
 fn main() -> Result<()> {
     let files = get_files()
         .wrap_err("Could not read files in data/input/ directory")?;
+    create_output_directories()
+        .wrap_err("Could not create output directories")?;
 
     for file in files {
         let content =
@@ -25,6 +28,8 @@ fn main() -> Result<()> {
         let expressions: Vec<Expression> = parsed_table
             .try_into()
             .wrap_err("Could not convert HTML table")?;
+        download_files(&expressions)
+            .wrap_err("Could not download audio files")?;
         let flashcards = expressions
             .into_iter()
             .map(Flashcard::try_from)
@@ -52,6 +57,13 @@ fn get_files() -> Result<Vec<PathBuf>> {
     Ok(files)
 }
 
+/// Creates `data/output/media` if it doesn't exist.
+fn create_output_directories() -> Result<()> {
+    fs::create_dir_all("data/output/media")
+        .wrap_err("Could not create data/output/media/")?;
+    Ok(())
+}
+
 /// Reads the file and removes no-break spaces (deserializer can't handle
 /// them).
 fn get_cleaned_content(file: &Path) -> Result<String> {
@@ -60,6 +72,33 @@ fn get_cleaned_content(file: &Path) -> Result<String> {
     let cleaned_content = str::replace(&original_content, "&nbsp;", "");
 
     Ok(cleaned_content)
+}
+
+/// Downloads the audio files for the expressions into `data/output/media`.
+fn download_files(expressions: &[Expression]) -> Result<()> {
+    let client = Client::new();
+
+    for expression in expressions {
+        let mut response = client
+            .get(expression.audio.clone())
+            .send()
+            .wrap_err("Could not download audio file")?;
+
+        let sound_file = expression
+            .audio
+            .path_segments()
+            .ok_or_else(|| eyre!("URL does not contain file"))?
+            .last()
+            .ok_or_else(|| eyre!("No last element in iterator"))?;
+        let mut output =
+            File::create(format!("data/output/media/{}", sound_file))
+                .wrap_err("Could not create file for audio")?;
+
+        io::copy(&mut response, &mut output)
+            .wrap_err("Could not copy content to file")?;
+    }
+
+    Ok(())
 }
 
 /// Returns the path to a CSV file with the same name as `input` in the output
